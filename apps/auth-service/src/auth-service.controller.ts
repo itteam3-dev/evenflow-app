a83589251f0221e9b32326a62a29605e5cef1b47
khaled_dev <it.team3@paytome.co>
2026-04-16 12:04:00 +0200

    build: register

--- apps/auth-service/src/auth-service.controller.ts
+++ apps/auth-service/src/auth-service.controller.ts
@@ -1,17 +1,7 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller } from '@nestjs/common';
 import { AuthServiceService } from './auth-service.service';
 
 @Controller()
 export class AuthServiceController {
   constructor(private readonly authServiceService: AuthServiceService) {}
-
-  @Get()
-  getHello(): string {
-    return this.authServiceService.getHello();
-  }
-
-  @Post('register')
-  registerUser(@Body() body: { email: string }) {
-    return this.authServiceService.simulateUserRegistration(body.email);
-  }
 }
--- apps/auth-service/src/auth-service.service.ts
+++ apps/auth-service/src/auth-service.service.ts
@@ -1,29 +1,55 @@
+import { DatabaseService, users } from '@app/database';
 import { KAFKA_SERVICE, KAFKA_TOPICS } from '@app/kafka';
-import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import {
+  ConflictException,
+  Inject,
+  Injectable,
+  OnModuleInit,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { ClientKafka } from '@nestjs/microservices';
+import * as bcrypt from 'bcrypt';
+import { eq } from 'drizzle-orm';
 
 @Injectable()
 export class AuthServiceService implements OnModuleInit {
   constructor(
-    @Inject(KAFKA_SERVICE)
-    private readonly kafkaClient: ClientKafka,
+    @Inject(KAFKA_SERVICE) private readonly kafkaClient: ClientKafka,
+    private readonly dbService: DatabaseService,
+    private readonly jwtService: JwtService,
   ) {}
 
   async onModuleInit() {
     await this.kafkaClient.connect();
   }
 
-  getHello(): string {
-    return 'Hello Auth module';
-  }
+  async register(email: string, password: string, name: string) {
+    const existingUser = await this.dbService.db
+      .select()
+      .from(users)
+      .where(eq(users.email, email))
+      .limit(1);
 
-  simulateUserRegistration(email: string) {
+    if (existingUser.length > 0) {
+      throw new ConflictException('User already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const [user] = await this.dbService.db
+      .insert(users)
+      .values({
+        email,
+        password: hashedPassword,
+        name,
+      })
+      .returning();
+
     this.kafkaClient.emit(KAFKA_TOPICS.USER_REGISTERED, {
-      email,
-      id: Math.floor(Math.random() * 1000),
-      createdAt: new Date().toISOString(),
+      email: user.email,
+      name: user.name,
     });
 
-    return { message: `User registered event sent ${email}` };
+    return { message: 'User registered successfully', userId: user.id };
   }
 }
